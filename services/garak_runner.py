89a208faab1daf1f4341e7607a47d896e1a580bc
Muktadirul.Islam <raiancse@gmail.com>
2025-08-01 03:01:48 +0600

    chore: add data filter and upload in s3 - upload filtered output in s3 - file upload after garak run

--- services/garak_runner.py
+++ services/garak_runner.py
@@ -1,7 +1,28 @@
 from garak.cli import main
 import subprocess
 import threading
+import os
+from aws import AwsS3
+from filter import LogFilter
+from pathlib import Path
 
-def run_garak_live(cmd, on_update):
-    CMD = "--model_type ollama --model_name phi3 --probes dan.AntiDAN --report_prefix dan.AntiDAN"
-    main(CMD.split(' '))
+
+aws_access_key = os.getenv('AWS_S3_ACCESS_KEY')
+aws_secret_key = os.getenv('AWS_S3_SECRET_KEY')
+
+aws_client = AwsS3(aws_access_key, aws_secret_key)
+cmd = '--model_type ollama --model_name phi3 --probes xss.MdExfil20230929 --report_prefix ollama.phi3.xss.MdExfil20230929.20250801'
+
+
+
+
+def run_garak_live(cmd):
+    main(cmd.split(' '))
+    log_filter = LogFilter(aws_client, cmd)
+    log_path = '/Users/panda/.local/share/garak/garak_runs/'
+    data_path = Path(f'{log_path}/ollama.phi3.xss.MdExfil20230929.20250801.report.jsonl')
+    data = log_filter.read_log_data(data_path)
+    log_filter.filtered_output_data(data)
+
+
+run_garak_live(cmd=cmd)
